757d3c4c2a3cbb31e1832c3bdee56beb7b5d63c3
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-11 20:18:21 +0500

    Python Operators

---
+++ PythonOperators.py
@@ -0,0 +1,46 @@
+# Python Operators
+
+# Arthimatic Operators
+a = 10
+b = 5
+
+print(a + b)    #sum
+print(a - b)    #subtract
+print(a * b)    #multiply
+print(a / b)    #divide
+print(a % b)    #reminder
+print(a ** b)   #power
+
+# Conditional Operators
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+# Assignment Operators
+number = 10
+
+number += 5
+print(number)
+number -= 10
+print(number)
+number *= 5
+print(number)
+number /= 10
+print(number)
+number %= 2.5
+print(number)
+number **= 50
+print(number)
+
+# Logical Operators AND OR and NOT
+c = 10
+d = 20
+
+print(not (c>d))
+print(not (c<d))
+
+print((c>d) and (c<d))
+print((c>d) or (c<d))
