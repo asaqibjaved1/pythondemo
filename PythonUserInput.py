13ba5cc2c50e5d0592f8a8e55a3aaa4e79737542
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-11 20:47:29 +0500

    Python User Inputs

---
+++ PythonUserInput.py
@@ -0,0 +1,9 @@
+# Getting user input in python
+
+name = input("Enter Your name please: ")
+age = int(input("Enter Your age please: "))
+
+print("Wellcome ", name)
+print("Your age is :" , age)
+
+print(type(age))
