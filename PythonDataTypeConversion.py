af83e560520bda31da408b686069a4a6132d574c
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-11 20:32:43 +0500

    Python Data Type Conversion/Casting

---
+++ PythonDataTypeConversion.py
@@ -0,0 +1,21 @@
+# Data Type conversion
+# Python do type conversion automatically to superior
+a = 5
+b = 10.5
+sum = a + b
+
+print(type(a))
+print(type(b))
+print(sum)
+print(type(sum))
+
+# Type Casting
+# When we do conversion manually, it is type casting
+
+c = float("2")
+d = 7.5
+sum = c + d
+print(type(c))
+print(type(d))
+print(sum)
+print(type(sum))
