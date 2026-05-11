83ff94fa5604393d4a98464a1684a563f53ec607
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-11 19:44:09 +0500

    Python Comments

---
+++ PythonComments.py
@@ -0,0 +1,11 @@
+# Python single line and multi line comments
+
+# Single line comment
+# I am Saqib
+# I am Leaning 
+
+"""
+Multi line Comments
+I am Saqib
+Triple quotation is used for multi line comments
+"""
