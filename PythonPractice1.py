86a53d046ae6fd3a34523d1c1d57b7addb3bc846
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-12 16:12:18 +0500

    Python Practice Exercise 1

---
+++ PythonPractice1.py
@@ -0,0 +1,9 @@
+# Python Practice 1
+# Write a Program to input 2 numbers & print their sum.
+
+print("We are going to add two numbers :")
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the Second number :"))
+sum = num1 + num2
+print("Sum of ",num1 ,  "and", num2 , "is : ",sum)
+
