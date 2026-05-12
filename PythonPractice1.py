f1e967d2620ba85551f12ae7bfbb72a86bba4f2f
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-12 16:34:42 +0500

    Practice questions

--- PythonPractice1.py
+++ PythonPractice1.py
@@ -1,9 +1,28 @@
-# Python Practice 1
-# Write a Program to input 2 numbers & print their sum.
+# # Python Practice 1
+# # Write a Program to input 2 numbers & print their sum.
 
 print("We are going to add two numbers :")
 num1 = int(input("Enter the first number :"))
 num2 = int(input("Enter the Second number :"))
 sum = num1 + num2
 print("Sum of ",num1 ,  "and", num2 , "is : ",sum)
+
+# WAP to input side of a square & print its area.
+print("We are going to find the area of a square by its side")
+side = int(input("Enter side of a square"))
+print ("Area of a square is : ", side * side)
+
+# WAP to input 2 floating point numbers & print their average.
+print("We are finding averagge of two numbers")
+num3 = float(input("Enter the first number : "))
+num4 = float(input("Enter the second number : "))
+print("Average of the two enter numbers is :" , (num3 + num4)/2)
+
+# WAP to input 2 int numbers, a and b.
+# Print True if a is greater than or equal to b. If not print False.
+print("Finding first is number is greater than the second or not")
+num5 = int(input("Enter the first number : "))
+num6 = int(input("Enter the second number : "))
+print(num5 >= num6) 
+
 
