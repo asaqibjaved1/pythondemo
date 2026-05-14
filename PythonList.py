ad93d9cd212fa22c6f8d46432151453543121df1
asaqibjaved1 <a.saqibjaved1@gmail.com>
2026-05-14 16:46:39 +0500

    Python lisgt

---
+++ PythonList.py
@@ -0,0 +1,16 @@
+# Python lista
+marks = [12, 13, 14, 15, 16, 17, 18, 20]
+print(marks)
+print(marks[0])
+print(marks[1])
+print(marks[2])
+print(marks[3])
+
+student = ["Saqib", 85, "Haripur"]
+print(student[0])
+print(student[1])
+print(student[2])
+
+print(type(student))
+print(len(student))
+
---
+++ PythonListSlicing.py
@@ -0,0 +1,5 @@
+# Python List Slicing
+Student = ["Saqib", 25, "Haripur", 55]
+print(Student[1:3])
+print(Student[0:])
+print(Student[:2])
